6e1e72a26eb2209e21ba0cbbf577209c0959a398
haitekie <haimanot.tekie@finn.no>
2019-11-21 00:13:01 +0100

    structure of album layout in place

---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Container() {
+    return <> 
+        <div class="container">
+            <Header />
+        </div>
+    </>
+}
+
+export default Container
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = function(){
+    return <>
+        <main role="main">
+            <section className="jumbotron text-center">
+                <div className="container">
+                    <h1 className="jumbotron-heading">Apostolic Church International</h1>
+                    <p className="lead text-muted">Then Peter said unto them, Repent, and be baptized every one of you in the name of Jesus Christ for the remission of sins, and ye shall receive the gift of the Holy Ghost.</p>
+                </div>
+            </section>
+        </main>
+    </>
+}
+
+
+export default Main
