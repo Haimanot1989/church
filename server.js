01da3d28f81f9c3e298a2252dfa8dc6f71c33ade
haimanot1989 <haimif@yahoo.com>
2020-05-17 23:26:41 +0200

    add env and server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+
+const app = express();
+
+app.use(compression());
+
+app.use('/static', express.static(process.env.ABSOLUTE_STATIC_PATH));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.env.ABSOLUTE_STATIC_PATH, '../index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', (err) => {
+  if (err) { console.log(err); }
+  console.info(`==> 🌎 app listening on http://localhost:${PORT}.`);
+});
