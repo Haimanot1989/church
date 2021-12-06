708303a70b971f25a2ecef4505d21e3c79205dc0
Haimanot Tekie <haimif@yahoo.com>
2021-12-06 23:06:20 +0100

    add sermons

--- src/utils/strManipulators.js
+++ src/utils/strManipulators.js
@@ -1,22 +1,41 @@
 function capitalizeTitle(title) {
-  return title
+  let cap = title
     .split(" ")
-    .map(str => `${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`)
+    .map(str => {
+      let capitalizedFirstLetter = [
+        "why",
+        "is",
+        "of",
+        "the",
+        "for",
+        "from",
+        "the",
+        "at"
+      ].includes(str.toLowerCase())
+        ? str.charAt(0).toLowerCase()
+        : str.charAt(0).toUpperCase();
+      return `${capitalizedFirstLetter}${str.slice(1).toLowerCase()}`;
+    })
     .join(" ");
+
+  return `${cap.charAt(0).toUpperCase()}${cap.slice(1)}`;
 }
 
 function convertTitleToFileName(title) {
   return title
+    .trim()
+    .replace(/[^0-9a-z ]/gi, "")
     .toUpperCase()
     .split(" ")
     .join("_");
 }
 
 let titles = [
-  "MANY GIFTS, ONE CHURCH",
-  "MODESTY IN DRESS",
-  "THE LAST GREAT CATCH OF FISH",
-  "THE MEDIATION OF CHRIST"
+  "SERVING GOD, SAVING THE KING ",
+  "ENCOURAGING MANOAH",
+  "THE PLAGUE STOPPED AT CALVARY",
+  "GOD’S WORD – OUR DAILY BREAD FROM HEAVEN",
+  "SIX REASONS WHY THE MYSTERY OF GODLINESS IS GREAT"
 ];
 titles.forEach(title => {
   console.log(
