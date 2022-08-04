488008abe5ebdc189e1b37f309875a8bc543d57c
Haimanot Tekie <haimif@yahoo.com>
2022-08-04 07:37:24 +0200

    update the conferences

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -22,11 +22,11 @@ const Cards = () => {
   return (
     <>
       <div className="card-deck mb-2">
+        <Card {...annualConference2022} />
         <OnlineMeetingCard {...europeBibleStudyOnZoom} />
-        <OnlineMeetingCard {...europeTigrinyaBibleStudyOnZoom} />
       </div>
       <div className="card-deck mb-2">
-        <Card {...annualConference2022} />
+        <OnlineMeetingCard {...europeTigrinyaBibleStudyOnZoom} />
         <Card {...youthConferenceBelgium2022} />
       </div>
       <div className="card-deck mb-2">
