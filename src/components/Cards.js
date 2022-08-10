fe3ee87d45d91e5fa03ffcdbc221206ddee8097e
Haimanot Tekie <haimif@yahoo.com>
2022-08-10 21:26:49 +0200

    update conferences

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,7 +13,6 @@ const Cards = () => {
   const europeTigrinyaBibleStudyOnZoom =
     onlineMeetings["europeTigrinyaBibleStudyOnZoom"];
   const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
-  const annualConference2022 = conferences["annualConference2022"];
   const youthConferenceBelgium2022 = conferences["youthConference2022"];
   const septemberConference2022 = conferences["septemberConference2022"];
   const octoberConference2022 = conferences["octoberConference2022"];
@@ -22,20 +21,16 @@ const Cards = () => {
   return (
     <>
       <div className="card-deck mb-2">
-        <Card {...annualConference2022} />
         <OnlineMeetingCard {...europeBibleStudyOnZoom} />
-      </div>
-      <div className="card-deck mb-2">
-        <OnlineMeetingCard {...europeTigrinyaBibleStudyOnZoom} />
         <Card {...youthConferenceBelgium2022} />
       </div>
       <div className="card-deck mb-2">
+        <OnlineMeetingCard {...europeTigrinyaBibleStudyOnZoom} />
         <Card {...septemberConference2022} />
-        <Card {...octoberConference2022} />
       </div>
       <div className="card-deck mb-2">
+        <Card {...octoberConference2022} />
         <Card {...novemberConference2022} />
-        <Card {...{}} />
       </div>
     </>
   );
