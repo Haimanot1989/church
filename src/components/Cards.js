b17eef3e414b561ac41825adbc44e099d2db58b8
Haimanot Tekie <haimif@yahoo.com>
2023-02-26 23:39:26 +0100

    annual conf 2023

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,12 +7,17 @@ import Card from "./Card";
 const Cards = () => {
   const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
   const abidjanConference2023 = conferences["abidjanConference2023"];
+  const annualConferenceNorway = conferences["annualConferenceNorway"];
 
   return (
     <>
       <div className="card-deck mb-2">
         <OnlineMeetingCard {...europeBibleStudyOnZoom} />
         <Card {...abidjanConference2023} />
+      </div>
+      <div className="card-deck mb-2">
+        <Card {...annualConferenceNorway} />
+        <Card {...{}} />
       </div>
     </>
   );
