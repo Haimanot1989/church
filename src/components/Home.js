5906545353c11c0dd0989be735950ddb7ed1b996
Haimanot Tekie <haimif@yahoo.com>
2023-06-03 17:03:54 +0200

    Remove announcements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { PageDescriber } from "./PageDescription";
-import AnnouncementCard from "./AnnouncementCard";
 import DoctrineOfSalvationIntroCard from "./DoctrineOfSalvationIntroCard";
-import announcements from "./data/announcement.json";
 import { OnlineMeetingCard } from "./OnlineMeetingCard";
 import Card from "./Card";
 import onlineMeetings from "./data/onlineMeetings.json";
 const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
 
 const Home = function() {
-  const nepal2023 = announcements["announcements"][0];
   let pageDescription = {
     title: "Jesus Alone is God Church of Europe",
   };
@@ -17,7 +14,6 @@ const Home = function() {
     <>
       <main role="main" className="container mb-2 position-main">
         <PageDescriber {...pageDescription} />
-        <AnnouncementCard {...nepal2023} />
         <DoctrineOfSalvationIntroCard />
         <div className="card-deck mb-2">
           <OnlineMeetingCard {...europeBibleStudyOnZoom} />
