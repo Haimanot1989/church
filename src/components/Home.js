46740ca0fbad8549b6bcc2f88b562546a50b45e4
Haimanot Tekie <haimif@yahoo.com>
2023-07-12 18:01:52 +0200

    Add a global report

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,12 @@ import DoctrineOfSalvationIntroCard from "./DoctrineOfSalvationIntroCard";
 import { OnlineMeetingCard } from "./OnlineMeetingCard";
 import Card from "./Card";
 import onlineMeetings from "./data/onlineMeetings.json";
+import AnnouncementCard from "./AnnouncementCard";
+import announcements from "./data/announcement.json";
 const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
 
 const Home = function() {
+  const firstAnnouncementElement = announcements["announcements"][0];
   let pageDescription = {
     title: "Jesus Alone is God Church of Europe",
   };
@@ -14,6 +17,7 @@ const Home = function() {
     <>
       <main role="main" className="container mb-2 position-main">
         <PageDescriber {...pageDescription} />
+        <AnnouncementCard {...firstAnnouncementElement} />
         <DoctrineOfSalvationIntroCard />
         <div className="card-deck mb-2">
           <OnlineMeetingCard {...europeBibleStudyOnZoom} />
