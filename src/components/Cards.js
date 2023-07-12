e408ff7c2e1d1f5e6a2f0dc024a35a6a452ef297
Haimanot Tekie <haimif@yahoo.com>
2023-07-12 18:11:33 +0200

    update conferences

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,14 +3,13 @@ import conferences from "./data/conferences.json";
 import Card from "./Card";
 
 const Cards = () => {
-  const abidjanConference2023 = conferences["abidjanConference2023"];
   const annualConferenceNorway = conferences["annualConferenceNorway"];
 
   return (
     <>
       <div className="card-deck mb-2">
-        <Card {...abidjanConference2023} />
         <Card {...annualConferenceNorway} />
+        <Card {...{}} />
       </div>
     </>
   );
