b1a96be85e2e0b66c7b27bcd7366fb098e5f96f6
Haimanot Tekie <haimif@yahoo.com>
2023-10-26 09:33:16 +0200

    update conferences

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,11 @@
 import React from "react";
-import conferences from "./data/conferences.json";
 import Card from "./Card";
 
 const Cards = () => {
-  const octoberConference2023 = conferences["octoberConference2023"];
-
   return (
     <>
       <div className="card-deck mb-2">
-        <Card {...octoberConference2023} />
+        <Card {...{}} />
       </div>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,11 @@ import React from "react";
 import { PageDescriber } from "./PageDescription";
 import DoctrineOfSalvationIntroCard from "./DoctrineOfSalvationIntroCard";
 import { OnlineMeetingCard } from "./OnlineMeetingCard";
-import Card from "./Card";
 import onlineMeetings from "./data/onlineMeetings.json";
-import conferences from "./data/conferences.json";
 
 const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
 
 const Home = function() {
-  const octoberConference2023 = conferences["octoberConference2023"];
   let pageDescription = {
     title: "Jesus Alone is God Church of Europe"
   };
@@ -17,9 +14,6 @@ const Home = function() {
     <>
       <main role="main" className="container mb-2 position-main">
         <PageDescriber {...pageDescription} />
-        <div className="card-deck mb-2">
-          <Card {...octoberConference2023} />
-        </div>
         <DoctrineOfSalvationIntroCard />
         <div className="card-deck mb-2">
           <OnlineMeetingCard {...europeBibleStudyOnZoom} />
