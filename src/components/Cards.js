76c5072e078d8495f24d069d372263501727c06d
Haimanot Tekie <haimif@yahoo.com>
2024-10-05 16:39:13 +0200

    update conference

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,12 +4,13 @@ import conferences from "./data/conferences.json";
 
 const Cards = () => {
   const octoberConference2024 = conferences["octoberConference2024"];
+  const newYearConference20242025 = conferences["newYearConference20242025"];
 
   return (
     <>
       <div className="card-deck mb-2">
         <Card {...octoberConference2024} />
-        <Card {...{}} />
+        <Card {...newYearConference20242025} />
       </div>
     </>
   );
