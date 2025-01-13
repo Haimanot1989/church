516ef2e24c3aec4184f1479b11dfbee37994f80c
Haimanot Tekie <haimif@yahoo.com>
2025-01-13 19:43:54 +0100

    update conference

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Card from "./Card";
+import conferences from "./data/conferences.json";
 
 const Cards = () => {
+  const februaryConferenceUK2025 = conferences["februaryConferenceUK2025"];
+
   return (
     <>
       <div className="card-deck mb-2">
+        <Card {...februaryConferenceUK2025} />
         <Card {...{}} />
       </div>
     </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,11 @@ import { PageDescriber } from "./PageDescription";
 import DoctrineOfSalvationIntroCard from "./DoctrineOfSalvationIntroCard";
 import { OnlineMeetingCard } from "./OnlineMeetingCard";
 import onlineMeetings from "./data/onlineMeetings.json";
+import conferences from "./data/conferences.json";
+import Card from "./Card";
 
 const europeBibleStudyOnZoom = onlineMeetings["europeBibleStudyOnZoom"];
+const februaryConferenceUK2025 = conferences["februaryConferenceUK2025"];
 
 const Home = function() {
   let pageDescription = {
@@ -17,6 +20,9 @@ const Home = function() {
         <DoctrineOfSalvationIntroCard />
         <div className="card-deck mb-2">
           <OnlineMeetingCard {...europeBibleStudyOnZoom} />
+        </div>
+        <div className="card-deck mb-2">
+          <Card {...februaryConferenceUK2025} />
         </div>
       </main>
     </>
