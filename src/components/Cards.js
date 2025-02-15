44a8e74f8d03983d8d651f992b7aac014e0ffd9d
Haimanot Tekie <haimif@yahoo.com>
2025-02-15 11:39:47 +0100

    update conf info

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,12 +4,13 @@ import conferences from "./data/conferences.json";
 
 const Cards = () => {
   const februaryConferenceUK2025 = conferences["februaryConferenceUK2025"];
+  const annualConference2025 = conferences["annualConference2025"];
 
   return (
     <>
       <div className="card-deck mb-2">
         <Card {...februaryConferenceUK2025} />
-        <Card {...{}} />
+        <Card {...annualConference2025} />
       </div>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -17,12 +17,12 @@ const Home = function() {
     <>
       <main role="main" className="container mb-2 position-main">
         <PageDescriber {...pageDescription} />
+        <div className="card-deck mb-2">
+          <Card {...februaryConferenceUK2025} />
+        </div>
         <DoctrineOfSalvationIntroCard />
         <div className="card-deck mb-2">
           <OnlineMeetingCard {...europeBibleStudyOnZoom} />
-        </div>
-        <div className="card-deck mb-2">
-          <Card {...februaryConferenceUK2025} />
         </div>
       </main>
     </>
