bb8f8fc3f9f0c34bd3e99553d8a8accaec012c2f
Haimanot Tekie <haimanot.tekie@nav.no>
2026-07-05 13:37:51 +0200

    update sermons, podcast and conferences

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,15 +3,15 @@ import Card from "./Card";
 import conferences from "./data/conferences.json";
 
 const Cards = () => {
-  const theLordSupperConferenceNorway2026 =
-    conferences["theLordSupperConferenceNorway2026"];
   const annualConferenceNorway = conferences["annualConferenceNorway"];
+  const harvestConference2026 =
+    conferences["harvestConference2026"];
 
   return (
     <>
       <div className="card-deck mb-2">
-        <Card {...theLordSupperConferenceNorway2026} />
         <Card {...annualConferenceNorway} />
+        <Card {...harvestConference2026} />
       </div>
     </>
   );
